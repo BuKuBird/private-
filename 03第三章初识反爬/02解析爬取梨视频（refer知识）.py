9062d0f66fa27145747e52dbc1f78ce183664010
buku <2297040821@qq.com>
2023-11-24 11:23:43 +0800

    202311241123

--- 03第三章初识反爬/02解析爬取梨视频（refer知识）.py
+++ 03第三章初识反爬/02解析爬取梨视频（refer知识）.py
@@ -1,9 +1,12 @@
 import requests
 
-contId = 1730846
-url1 = "https://www.pearvideo.com/video_1730846"
-url = f"https://www.pearvideo.com/videoStatus.jsp?contId={contId}&mrd=0.11070613714328803"
+# 注意在创建连接的时候不能开代理，不然会报错
 
+
+contId = 1789267
+url1 = "https://www.pearvideo.com/video_1789267"
+url = f"https://www.pearvideo.com/videoStatus.jsp?contId=1789267&mrd=0.6269419464271935"
+print(url)
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/119.0",
     "Referer": url1
@@ -19,7 +22,23 @@ def socketor(url, charset):
     resp.close()
     return resp
 
+# 需要的url
+# https://video.pearvideo.com/mp4/short/20231115/cont-1789267-16008473-hd.mp4
+# https://video.pearvideo.com/mp4/short/20231115/cont-1789267--16008473-hd.mp4
+# https://video.pearvideo.com/mp4/short/20231115/1700794587154-16008473-hd.mp4
+# 获得的url
+# https://video.pearvideo.com/mp4/short/20231115/ 1700793758721-16008473-hd.mp4
+
 resp = socketor(url, "utf-8")
+dic = resp.json()
 srcUrl = str(resp.json()["videoInfo"]["videos"]["srcUrl"])
-srcUrl.strip("-",)
-print(srcUrl)
+
+systemTime = dic["systemTime"]
+inst = srcUrl.split(f"{systemTime}")
+# print(inst[0])
+# ['https://video.pearvideo.com/mp4/short/20231115/', '-16008473-hd.mp4']
+provideosrc = inst[0] + f'cont-{1789267}' + inst[1]
+# print(provideosrc)
+# print(srcUrl)
+with open("1.mp4",mode="wb") as f: # 在wb模式下不能用encoding
+    f.write(requests.get(provideosrc).content)
