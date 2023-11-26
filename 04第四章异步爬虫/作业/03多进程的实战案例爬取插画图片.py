a5fa2bcbb9547a6029de2e8682fb0dee48ea0ed3
buku <2297040821@qq.com>
2023-11-26 20:50:51 +0800

    202311262050

--- 04第四章异步爬虫/作业/03多进程的实战案例爬取插画图片.py
+++ 04第四章异步爬虫/作业/03多进程的实战案例爬取插画图片.py
@@ -6,3 +6,35 @@
 
 
 """
+import requests
+import urllib
+from lxml import etree
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/119.0"
+}
+def socketor(url, charset='gbk'):
+    """
+    发送一个get请求返回源代码
+    :param url: url地址
+    :return: source_code
+    """
+    resp = requests.get(url, headers=headers)
+    resp.encoding = charset
+    resp.close()
+    return resp
+
+def get_src():
+    mainurl = f"http://www.yuanhua.org/yuanhua.php?hid=3&indexkey=%C6%E6%BB%C3%B2%E5%BB%AD&page=1"
+    resp = socketor(url=mainurl)
+    # print(resp.text)
+    tree = etree.HTML(resp.text)
+    page_num = tree.xpath('//tr[@bgcolor="#F8F8F8"]/td/text()')[1]
+
+    # table_body = tree.xpath("//table")[2]
+
+    print(page_num)
+def down_load_img():
+    pass
+
+if __name__ == '__main__':
+    get_src()
